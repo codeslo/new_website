565b84c5e6112fbdf40584eba138eee6072e0c2a
John Jenson <JRRJenson@gmail.com>
2018-10-28 12:31:45 -0700

    add keystone user model

--- keystone.js
+++ keystone.js
@@ -1,7 +1,12 @@
 var keystone = require('keystone');
 
 keystone.init({
-  'cookie secret': 'secure string goes here'
+  'cookie secret': 'secure string goes here',
+  'name': 'new-website',
+  'user model': 'User',
+  'auto update': true,
+  'auth': true,
 });
 
+keystone.import('models');
 keystone.start();
