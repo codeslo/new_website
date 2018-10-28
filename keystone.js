fd2fd664d3895cd1fabc9a910c38394b9ecf8de3
matt-codeslo <matt@codeslo.com>
2018-10-28 12:41:30 -0700

    delete test files

--- keystone.js
+++ keystone.js
@@ -1,7 +1,9 @@
-var keystone = require('keystone');
+const keystone = require('keystone');
+const ejs = require('ejs');
 
 keystone.init({
-  'cookie secret': 'secure string goes here'
+  'cookie secret': 'secure string goes here',
+  'view engine': 'ejs'
 });
 
 keystone.start();
