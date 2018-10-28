34935bc22a8316d309cae6acac10e51611ef1def
John Jenson <JRRJenson@gmail.com>
2018-10-28 13:36:17 -0700

    add routes and views

--- keystone.js
+++ keystone.js
@@ -1,14 +1,17 @@
 const keystone = require('keystone');
-const ejs = require('ejs');
+//const ejs = require('ejs');
 
 keystone.init({
   'cookie secret': 'secure string goes here',
-  'view engine': 'ejs',
   'name': 'new-website',
+  'cookie secret': 'secure string goes here',
   'user model': 'User',
   'auto update': true,
   'auth': true,
+	views: 'templates/views',
+	'view engine': 'ejs',
 });
 
 keystone.import('models');
+keystone.set('routes', require('./routes'));
 keystone.start();
