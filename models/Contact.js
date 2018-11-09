43396a4ee92977de05aa16a2557f09beb92438e7
John Jenson <JRRJenson@gmail.com>
2018-11-08 20:54:29 -0800

    connect contact form to controller

--- models/Contact.js
+++ models/Contact.js
@@ -4,7 +4,7 @@ var Contact = new keystone.List('Contact');
 
 Contact.add({
   name: { type: keystone.Field.Types.Name, required: true, initial: true },
-  email: { type: keystone.Field.Types.Email, required: true, initial: true, unique: true },
+  email: { type: keystone.Field.Types.Email, required: true, initial: true },
   message: { type: keystone.Field.Types.Textarea, required: true, initial: true }
 });
 
--- routes/api/contact/post.js
+++ routes/api/contact/post.js
@@ -2,10 +2,6 @@ var keystone = require('keystone');
 var Contact = keystone.list('Contact');
 
 module.exports = function (req, res) {
-  if (!req.body.first || !req.body.last || !req.body.email) {
-    return res.send({ status: 'incomplete data set' });
-  }
-
   req.body.name = {};
   req.body.name.first = req.body.first;
   req.body.name.last = req.body.last;
@@ -13,9 +9,10 @@ module.exports = function (req, res) {
   var newContact = new Contact.model();
   console.log(newContact, req.body);
   Contact.updateItem(newContact, req.body, function (error) {
-    res.locals.enquirySubmitted = true;
-    if (error) res.locals.saveError = true;
-    console.log(error);
-    res.render('addContact');
+    if (error) {
+      console.log(error);
+      return res.send({ message: 'Error' });
+    }
+    return res.send({ message: 'Message Sent!' });
   });
 };
