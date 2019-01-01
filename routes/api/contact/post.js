31a7c81f858993cd645b21bbbc2e86398f5b48f8
matt-codeslo <matt@codeslo.com>
2019-01-01 14:24:17 -0800

    changes to post.js

--- routes/api/contact/post.js
+++ routes/api/contact/post.js
@@ -3,18 +3,23 @@ var Contact = keystone.list('Contact');
 
 // save contact to DB
 
-// email contact to matt
-
 module.exports = function (req, res) {
-  // req.body.name = {};
-  // req.body.name.first = req.body.first;
-  // req.body.name.last = req.body.last;
+  if(!req.body.contactform.botcheck){
+    let contact = {};
+    contact.FirstName = req.body.contactform-first;
+    contact.LastName = req.body.contactform-last;
+    contact.Email = req.body.contactform-email;
+    contact.Phone = req.body.contactform-tel;
+    contact.Subscribed = req.body.contactform-subscribe;
+    contact.ContactType = req.body.contactform-ind-org;
+    contact.OrgName = req.body.contactform-org-name;
+  }else{
+    console.log("Bot contact detected. No DB operations performed.");
+  }
   
-  
-
   var newContact = new Contact.model();
-  console.log(newContact, req.body);
-  Contact.updateItem(newContact, req.body, function (error) {
+  console.log(newContact, contact);
+  Contact.updateItem(newContact, contact, function (error) {
     if (error) {
       console.log(error);
       return res.send({ message: 'Error' });
@@ -22,3 +27,5 @@ module.exports = function (req, res) {
     return res.send({ message: 'Message Sent!' });
   });
 };
+
+// email contact to matt
