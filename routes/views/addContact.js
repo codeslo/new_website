76b3dc485a855446c6abdce97fc7c0b0c879c26b
Lynne West <thislynnewest@gmail.com>
2019-01-08 14:44:32 -0800

    testing

--- routes/views/addContact.js
+++ routes/views/addContact.js
@@ -1,4 +1,4 @@
-module.exports = function (req, res) {
-  res.locals.enquirySubmitted = false;
-  res.render('addContact');
-};
+// module.exports = function (req, res) {
+//   res.locals.enquirySubmitted = false;
+//   res.render('addContact');
+// };
