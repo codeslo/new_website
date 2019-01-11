36e88a91599c1f6ffd73d262226b7234e54dceec
Lynne West <thislynnewest@gmail.com>
2019-01-11 10:41:25 -0800

    Configured nodemailer for contact form messages

--- routes/api/contact/post.js
+++ routes/api/contact/post.js
@@ -1,5 +1,9 @@
-var keystone = require('keystone');
-var Contact = keystone.list('Contact');
+const keystone = require('keystone');
+const nodemailer = require('nodemailer');
+if(process.env.NODE_ENV !== 'production') {
+  var config = require('../../../secrets');  
+}
+const Contact = keystone.list('Contact');
 
 // save contact to DB
 module.exports = function (req, res) {
@@ -22,13 +26,41 @@ module.exports = function (req, res) {
   Contact.updateItem(newContact, contact, function (error) {
     if (error) {
       console.log(error);
-      req.flash('error', 'Something went wrong. You can email us at info@codeslo.com')
+      req.flash('error', 'Something went wrong. You can email us at info@codeslo.com');
       return res.redirect("/");
     }
     req.flash('success', 'We got your message and we\'ll respond soon. Thanks for contacting CodeSLO.');
     return  res.redirect("/");
     //return res.send({data:"got message"})    
-  });  
+  });
+  
+  // email contact to matt
+  var transporter = nodemailer.createTransport({  
+    service: 'gmail',
+    auth: {
+           user: process.env.NODEMAILER_USER || config.NODEMAILER_USER,
+           pass: process.env.NODEMAILER_PASS || config.NODEMAILER_PASS
+       }
+   });
+
+  const mailOptions = {
+    from: req.body.contactform_email,
+    replyTo: req.body.contactform_email,
+    to: 'codeslotest@gmail.com',
+    envelope: {
+      from: req.body.contactform_email
+    }, 
+    subject: 'Email from CodeSLO contact form', 
+    html: '<h3>From: </h3>'+req.body.contactform_first+' '+req.body.contactform_last+'<h3>Email: </h3>'+ req.body.contactform_email+'<h3>Message: </h3>'+req.body.contactform_message
+  };  
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if(error){
+      return console.log(error);
+    }
+    console.log(info.response);
+
+  });
+
 };
 
-// email contact to matt
