967e66093bb6d8460c584d1b0a4f8ac7ecf46acd
Lynne West <thislynnewest@gmail.com>
2019-01-20 09:55:43 -0800

    Minor visual changes

--- routes/views/article.js
+++ routes/views/article.js
@@ -4,5 +4,6 @@ module.exports = function (req, res) {
   let view = new keystone.View(req,res);  
   view.query('news',keystone.list('News').model.findOne({slug: req.params.slug}, function(err, article) {
     view.render('article',{article:article});
-  }));  
+  }));
+    
 };
