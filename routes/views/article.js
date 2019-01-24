c4fb26798e51fc6cd1ef4a27593ef7f0b95aa1cb
Lynne West <thislynnewest@gmail.com>
2019-01-24 10:12:56 -0800

    Added open graph protocol meta tags

--- routes/views/article.js
+++ routes/views/article.js
@@ -1,16 +1,16 @@
 const keystone = require('keystone');
 
 module.exports = function (req, res) {
-  let view = new keystone.View(req,res);  
+  let view = new keystone.View(req,res);
+  locals = res.locals;  
   view.query('news',keystone.list('News').model.findOne({slug: req.params.slug}, function(err, article) {
     if(err){
       console.log(err)
       req.flash('error', 'Something went wrong, we can\'t find that article. Sorry about that!')
       return res.redirect("/")
-    } else{
+    }else {
       view.render('article',{article:article});
-    }
-    
+    }    
   }));
-    
+  locals.view = 'article-view';  
 };
