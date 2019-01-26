1c66b0a753e5bb539ca95ac6f442811fc0081b2a
Lynne West <thislynnewest@gmail.com>
2019-01-25 16:29:22 -0800

    Add meta description for article sharing

--- models/News.js
+++ models/News.js
@@ -6,8 +6,9 @@ News.add({
     slug: {type: String,noedit:true,unique:true},
     Image:{type:Types.CloudinaryImage,required:true,initial:true},
     Title:{type:Types.Text,required:true,initial:true},
-    Content:{type:Types.Html, wysiwyg:true,required:true,initial:true},    
-    createdAt: {type:Types.Date, default:Date.now(),required:true,initial:true}    
+    Content:{type:Types.Html, wysiwyg:true,required:true,initial:true},
+    ShareDescription: {type:Types.Textarea, height:80, required:true, initial:true, note: "A clear description that entices users to click, at least two sentences long."},    
+    createdAt: {type:Types.Date, default:Date.now(),required:true,initial:true}       
   }); 
 
   News.defaultColumns = 'createdAt, Title';
